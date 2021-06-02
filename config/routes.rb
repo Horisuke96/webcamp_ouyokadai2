fa16df74594a9aec1a3e43642622c53a4fac7f8a
Horisuke96 <horisuke_52m@yahoo.co.jp>
2021-06-02 06:25:32 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books
+
+  root 'homes#top'
+  get 'homes/about' => 'homes#about'
+end
